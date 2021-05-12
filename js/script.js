1c954689ee684502e1f527e0fa2d5bd44cefc255
Sam Louis Walker-Penn <siesta@sams-air.home>
2021-05-12 17:11:08 +0100

    Add employees table

--- js/script.js
+++ js/script.js
@@ -15,6 +15,15 @@ function ajaxRequest(url, data, success) {
 $(window).on("load", function () {
   const url = "libs/php/getAll.php";
   const success = function (result) {
+    $.each(result.data, function (i, o) {
+      const first = "<td>" + o.firstName + "</td>";
+      const last = "<td>" + o.lastName + "</td>";
+      const department = "<td>" + o.department + "</td>";
+      const location = "<td>" + o.location + "</td>";
+      const row = "<tr>" + first + last + department + location + "</tr>";
+      $("#employees").append(row);
+    });
+
     console.log(result);
   };
   ajaxRequest(url, {}, success);
